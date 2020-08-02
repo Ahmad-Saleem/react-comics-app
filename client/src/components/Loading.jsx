3a71d884ae11e601d98a2aef1ef0808da96841f6
Ahmad <it_saleem/2hotmail.com>
2020-08-03 00:16:33 +0100

    improve loading

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledLoading = styled.div`
+  position: absolute;
+  width: 200px;
+  padding: 16px;
+  border-radius: 8px;
+  border: 2px solid #777;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  backdrop-filter: blur(5px);
+  font-size: 18px;
+  color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export default function Loading() {
-  return <div>Loading...</div>
+  return <StyledLoading>Loading...</StyledLoading>
 }
